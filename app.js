a76d03fe90ce66064e2f6aadec699b69c2ae3727
basant <basant.b.hassan@gmail.com>
2019-01-06 17:30:36 +0200

    react

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose')
 
+mongoose.connect("mongodb://basant:basant11@ds061938.mlab.com:61938/server")
+mongoose.connection.once('open',()=>{
+    console.log('connected to database');
+})
 
 const app =  express();
 app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
 
 }))
 
 app.listen(4000, ()=>{
-    console.log("now listenning for request 4000")
+    console.log("now listenning for request 3000")
 })
